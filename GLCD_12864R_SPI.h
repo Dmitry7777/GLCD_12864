d52be64521ca222d7e205842e1a276edecaf87be
Dmitry7777 <list-dmitrij@mail.ru>
2019-08-30 23:51:02 +0600

    Update GLCD_12864R_SPI.h

--- GLCD_12864R_SPI.h
+++ GLCD_12864R_SPI.h
@@ -11,30 +11,44 @@ class GLCD_12864R_SPI{
   public:
   GLCD_12864R_SPI(int RS, int RW, int EN){
     }
- void Driver(){
-    
+ void Driver(int CLK, int LATCH, int SDA){
+    PROCESS_GLCD_RS = CLK;
+    PROCESS_GLCD_RW = LATCH;
+    PROCESS_GLCD_EN = SDA;
   };
  void init(void){
+  pinMode(PROCESS_GLCD_RS, IO_MCU_GLCD_RS); //
+  pinMode(PROCESS_GLCD_RW, IO_MCU_GLCD_RW); //
+  pinMode(PROCESS_GLCD_EN, IO_MCU_GLCD_EN); //
+  digitalWrite(PROCESS_GLCD_RW, LOW); //
+  //digitalWrite(); //
+  //digitalWrite(); //
+  Delays(); //
+  WriteCommand(0x30);
+  WriteCommand(0x0C);
+  WriteCommand(0x01);
+  WriteCommand(0x06);
       };
 
  void Delays(){
       
     }
 
- void WriteByte(){
+ void WriteByte(int DATA){
   
  }
 
- void WriteCommand(){
+ void WriteCommand(int COMMAND){
   
  }
 
-void WriteData(){
+void WriteData(int COMMAND){
   
 }
 
 void clear(){
-  
+WriteCommand(0x30); //
+WriteCommand(0x01); //
 }
 
 void DisplayString(){
@@ -59,14 +73,16 @@ void Image2(){
 
 int DelayTime; //
 int DefaultTime; //
+
+int PROCESS_GLCD_RS;
+int PROCESS_GLCD_RW;
+int PROCESS_GLCD_EN;
   
   private:
   
   protected:
 
-int PROCESS_GLCD_RS;
-int PROCESS_GLCD_RW;
-int PROCESS_GLCD_EN;
+
 };
 
 #endif
