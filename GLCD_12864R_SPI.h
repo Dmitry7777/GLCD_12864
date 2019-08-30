8d00a807d91b30e108c2a516caba8a9cbb7dbfb5
Dmitry7777 <list-dmitrij@mail.ru>
2019-08-30 10:20:11 +0600

    Update GLCD_12864R_SPI.h

--- GLCD_12864R_SPI.h
+++ GLCD_12864R_SPI.h
@@ -1,4 +1,24 @@
 #ifndef GLCD_12864R_SPI_H
 #define GLCD_12864R_SPI_H
 
+#define IO_MCU_GLCD_RS OUTPUT
+#define IO_MCU_GLCD_RW OUTPUT
+#define IO_MCU_GLCD_EN OUTPUT
+
+//int PROCESS_GLCD_RS;
+//int PROCESS_GLCD_RW;
+//int PROCESS_GLCD_EN;
+
+class GLCD_12864R_SPI{
+  public:
+  GLCD_12864R_SPI(int RS, int RW, int EN){
+    }
+    void init(){
+      };
+  
+  private:
+  
+  protected:
+};
+
 #endif
